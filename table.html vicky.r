e6ae93a1b8f094ed41488ca4d4ef4e6d468013a9
vignesh200523 <vigneshrajalingam23@gmail.com>
2024-08-20 15:19:37 +0530

    Add files via upload

---
+++ table.html vicky.r
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+      <title>table></title>
+      < style>
+               table,tb,th{
+                      border:2px solid blue;
+                      border-colapse: collapse;
+}
+     </style>
+</head>
+<body>
+         <table> 
+                  <tr>
+                         <th>frist name</th>
+                         <th>last name</th>
+                         <th>age></th>
+                  </tr>
+                  <tr>
+                          <tr>elam</td>
+                          <td>christopher</td>
+                          <td>18</td>
+                   </tr>
+                   <tr>
+                          <td>maha</td>
+                          <td>sri</td>
+                          <td>17</td>
+                   </tr>
+         </table>
+</body>
